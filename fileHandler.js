b4d33ad02c57337f45b5363a01ac5590e215832c
Nikhil Marne <marne.nikhil@outlook.com>
2025-12-28 07:13:23 +0530

    feat: implement FileHandler with auto-initialization

---
+++ fileHandler.js
@@ -0,0 +1,43 @@
+import fs from "node:fs"
+import path from "node:path"
+
+export class fileHanlder {
+    BASE_DIR;
+    FILES_DIR;
+    USER_FILE
+
+    constructor() {
+        this.BASE_DIR = path.resolve(".");
+        this.FILES_DIR = path.join(this.BASE_DIR, "files");
+        this.USER_FILE = path.join(this.FILES_DIR, "user.json")
+
+        this.init();
+    }
+
+    init() {
+        try {
+            // CHECK IF STORAGE DIR EXISTS IF NOT CREATE
+            if (!fs.existsSync(this.FILES_DIR)) {
+                fs.mkdirSync(this.FILES_DIR, { recursive: true })
+            }
+
+            // CHECK IF USER EXPNESES FILE EXISTS IF NOT CREATE INITIAL FILE
+            if (!fs.existsSync(this.USER_FILE)) {
+                this.writeData({
+                    nextId: 1,
+                    expenses: {}
+                })
+            }
+        } catch (err) {
+            console.log(`Application Error: ${err.message}`)
+        }
+    }
+
+    writeData(data) {
+        try {
+            fs.writeFileSync(this.USER_FILE, JSON.stringify(data, null, 2), "utf-8")
+        } catch (err) {
+            console.log(`Application Error: ${err.message}`)
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Expense Tracker CLI")
+
+
+const handlers = {
+    add: () => { },
+    update: () => { },
+    delete: () => { },
+    summary: () => { },
+    list: () => { }
+}
+
+
+
+const args = process.argv.slice(2)
+
+if (args.length) {
+
+} else {
+    console.log("No input command.")
+}
